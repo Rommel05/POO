2b6caabda88de7cb35301ed96e2c020906364745
Rommel05 <rrll20220@gmail.com>
2024-02-05 10:14:04 +0100

    ejercicios POO

--- src/Clase/Main.java
+++ src/Clase/Main.java
@@ -7,8 +7,6 @@ public class Main {
                 Facultad fac1 = new Facultad(13, "Facultad1");
                 Catedra catedra1 = new Catedra("Catedra1",dep1, fac1);
                     Professor profe1 = new Professor(12, dep1);
-                    catedra1.addProfe(profe1);
-                    profe1.addCatedras(catedra1);
         for (Departmento departamento: area1.getDepartmentos()) {
             for (Professor profe:departamento.getProfessores()) {
                 System.out.println(profe);
