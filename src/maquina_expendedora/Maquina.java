42d966636e4ee81a554999d20373bf1a507d80e9
Rommel05 <rrll20220@gmail.com>
2024-02-08 08:31:56 +0100

    ejercicios POO

--- src/maquina_expendedora/Maquina.java
+++ src/maquina_expendedora/Maquina.java
@@ -1,37 +1,39 @@
 package maquina_expendedora;
 
-public class Maquina {
-    private int tique;
-    private int dineroIntroducido;
-    private int cajon;
+import java.util.ArrayList;
 
-    public Maquina(int tique, int cajon) {
+public class Maquina {
+    private ArrayList<Double> tique;
+    private double dineroIntroducido;
+    private ArrayList<Double> cajon;
+
+    public Maquina(ArrayList<Double> tique, ArrayList<Double> cajon) {
         this.tique = tique;
         this.cajon = cajon;
     }
 
-    public int getCajon() {
-        return cajon;
-    }
-
-    public void setCajon(int cajon) {
-        this.cajon = cajon;
-    }
-
-    public int getTique() {
+    public ArrayList<Double> getTique() {
         return tique;
     }
 
-    public void setTique(int tique) {
+    public void setTique(ArrayList<Double> tique) {
         this.tique = tique;
     }
 
-    public int getDineroIntroducido() {
+    public double getDineroIntroducido() {
         return dineroIntroducido;
     }
 
-    public void setDineroIntroducido(int dineroIntroducido) {
+    public void setDineroIntroducido(double dineroIntroducido) {
         this.dineroIntroducido = dineroIntroducido;
+    }
+
+    public ArrayList<Double> getCajon() {
+        return cajon;
+    }
+
+    public void setCajon(ArrayList<Double> cajon) {
+        this.cajon = cajon;
     }
 
     @Override
