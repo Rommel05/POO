5c092aa82777dcf4006042aa265265b8c9050106
Rommel05 <rrll20220@gmail.com>
2024-02-15 09:46:42 +0100

    ejercicios POO

--- src/trabajadores/Oficial.java
+++ src/trabajadores/Oficial.java
@@ -4,4 +4,9 @@ public class Oficial extends Operario{
     public Oficial(String nombre) {
         super(nombre);
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/trabajadores/Tecnico.java
+++ src/trabajadores/Tecnico.java
@@ -4,4 +4,9 @@ public class Tecnico extends Operario{
     public Tecnico(String nombre) {
         super(nombre);
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
